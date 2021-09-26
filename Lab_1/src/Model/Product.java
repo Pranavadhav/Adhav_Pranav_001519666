86609c524424054548fb83e6ef7cee2853a284a5
Pranavadhav <adhav.p@northeastern.edu>
2021-09-27 04:54:01 +0530

    commiting files

---
+++ Lab_1/src/Model/Product.java
@@ -0,0 +1,185 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.io.File;
+
+/**
+ *
+ * @author pranav
+ */
+public class Product {
+    private String name;
+    private String address;
+    private String date;
+    private long phoneNumber ;
+    private long faxNumber;
+    private String email;
+    private String socialSecurityNumber;
+    private String medicalRecordNumber;
+    private String healthPlanBenificiaryNumber;
+    private int bankAccountNumber;
+    private String certificateNumber;
+    private String vehicalSerialNumber;
+    private String deviceSerialNumber;
+    private String linkedin;
+    private String internetProtocol;
+    private String biomertic;
+    private File photo;
+    private String uniqueIndentifyingNumber;
+
+    
+            
+            
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public long getFaxNumber() {
+        return faxNumber;
+    }
+
+    public void setFaxNumber(long faxNumber) {
+        this.faxNumber = faxNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSocialSecurityNumber() {
+        return socialSecurityNumber;
+    }
+
+    public void setSocialSecurityNumber(String socialSecurityNumber) {
+        this.socialSecurityNumber = socialSecurityNumber;
+    }
+
+    public String getMedicalRecordNumber() {
+        return medicalRecordNumber;
+    }
+
+    public void setMedicalRecordNumber(String medicalRecordNumber) {
+        this.medicalRecordNumber = medicalRecordNumber;
+    }
+
+    public String getHealthPlanBenificiaryNumber() {
+        return healthPlanBenificiaryNumber;
+    }
+
+    public void setHealthPlanBenificiaryNumber(String healthPlanBenificiaryNumber) {
+        this.healthPlanBenificiaryNumber = healthPlanBenificiaryNumber;
+    }
+
+    public int getBankAccountNumber() {
+        return bankAccountNumber;
+    }
+
+    public void setBankAccountNumber(int bankAccountNumber) {
+        this.bankAccountNumber = bankAccountNumber;
+    }
+
+    public String getCertificateNumber() {
+        return certificateNumber;
+    }
+
+    public void setCertificateNumber(String certificateNumber) {
+        this.certificateNumber = certificateNumber;
+    }
+
+    public String getVehicalSerialNumber() {
+        return vehicalSerialNumber;
+    }
+
+    public void setVehicalSerialNumber(String vehicalSerialNumber) {
+        this.vehicalSerialNumber = vehicalSerialNumber;
+    }
+
+    public String getDeviceSerialNumber() {
+        return deviceSerialNumber;
+    }
+
+    public void setDeviceSerialNumber(String deviceSerialNumber) {
+        this.deviceSerialNumber = deviceSerialNumber;
+    }
+
+    public String getLinkedin() {
+        return linkedin;
+    }
+
+    public void setLinkedin(String linkedin) {
+        this.linkedin = linkedin;
+    }
+
+    public String getInternetProtocol() {
+        return internetProtocol;
+    }
+
+    public void setInternetProtocol(String internetProtocol) {
+        this.internetProtocol = internetProtocol;
+    }
+
+    public String getBiomertic() {
+        return biomertic;
+    }
+
+    public void setBiomertic(String biomertic) {
+        this.biomertic = biomertic;
+    }
+
+    public File getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(File photo) {
+        this.photo = photo;
+     }
+
+    public String getUniqueIndentifyingNumber() {
+        return uniqueIndentifyingNumber;
+    }
+
+    public void setUniqueIndentifyingNumber(String uniqueIndentifyingNumber) {
+        this.uniqueIndentifyingNumber = uniqueIndentifyingNumber;
+    }
+    
+      
+    
+    
+    
+}
